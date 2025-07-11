4dac557911d38426e5f11e24274df7e43bf6f444
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-11 12:57:46 +0600

    axios

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
-    const location = useLocation()
+    const location = useLocation();
     if (loading) {
         return (
             <div className="text-center mt-10"> <span className="loading loading-dots loading-md"></span>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,41 +1,44 @@
-import axios from "axios";
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../Provider/Authprovider";
+import axios from 'axios'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../Provider/Authprovider'
 
 const axiosSecure = axios.create({
   baseURL: 'https://tourism-management-server-ten.vercel.app'
 })
+
 const useAxiosSecure = () => {
-    const navigate = useNavigate();
-    const { logOut } = useContext(AuthContext)
+  const navigate = useNavigate();
+  const { logOut } = useContext(AuthContext);
 
-    // request interceptor to add authorization header for every secure call to teh api
-    axiosSecure.interceptors.request.use(function (config) {
-        const token = localStorage.getItem('access-token')
-        console.log('request stopped by interceptors', token)
-        config.headers.authorization = `Bearer ${token}`;
-        return config;
-    }, function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    });
 
-    // intercepts 401 and 403 status
-    axiosSecure.interceptors.response.use(function (response) {
-        return response;
-    }, async (error) => {
-        const status = error.response.status;
-        console.log('status error in the interceptor', status);
-        // for 401 or 403 logout the user and move the user to the login
-        if (status === 401 || status === 403) {
-            await logOut();
-            navigate('/auth/login');
-        }
-        return Promise.reject(error);
-    })
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token')
 
-    return axiosSecure;
-};
+    console.log('request  stopped by interceptors', token)
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  })
+
+
+  // intercepets 401 and 403 ststus
+  axiosSecure.interceptors.response.use(function (response) {
+    return response;
+  }, async (error) => {
+    const status = error.response.status
+    console.log('status error in the interceptors', status)
+    
+    if (status === 401 || 403) {
+      // await logOut();
+      // navigate('/');
+    }
+
+    return Promise.reject(error);
+  })
+
+  return axiosSecure;
+}
 
 export default useAxiosSecure;
